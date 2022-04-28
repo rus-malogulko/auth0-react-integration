a6ca4c015e87f7ac2e5c378536a2e9913c734622
Ruslan Malogulko <ruslan.malogulko@verygoodsecurity.com>
2022-04-29 00:05:25 +0300

    Implement login/logout with auth0-react hook

--- src/app.js
+++ src/app.js
@@ -3,10 +3,19 @@ import { Route, Switch } from "react-router-dom";
 
 import { NavBar, Footer } from "./components";
 import { Home, Profile, ExternalApi } from "./views";
+import { Loading } from './components';
+
+import { useAuth0 } from '@auth0/auth0-react';
 
 import "./app.css";
 
 const App = () => {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <div id="app" className="d-flex flex-column h-100">
       <NavBar />
